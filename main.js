39b39911cc629150eef19a7df327dbcfd0728631
Glen Oakley <goakley123@gmail.com>
2012-12-21 00:27:10 +0000

    Enabled right-clicking on map to enter location

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-
-    if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-	    var current_value = $("#form_location_text").val();
-	    if (current_value.length == 0) {
-		$("#form_location_text").val(position.coords.latitude + "," + 
-					     position.coords.longitude);
-	    }
-	});
-    }
+// set up the map
 var map = new google.maps.Map(document.getElementById('map'),
 			      {center:(new google.maps.LatLng(0,0)),
 			       zoom:3,
 			       mapTypeId:google.maps.MapTypeId.HYBRID});
 google.maps.event.trigger(map, 'resize', function(){});
+google.maps.event.addListener(map, 'rightclick', function(event){
+    event.stop();
+    $("#form_location_text").val(""+event.latLng.lat()+","+event.latLng.lng());
+});
+
+// obtain geolocation data if possible and handle
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+	var current_value = $("#form_location_text").val();
+	map.panTo(new google.maps.LatLng(position.coords.latitude,
+					 position.coords.longitude,
+					 false));
+	map.setZoom(8);
+    });
+}
+
 
 var oms = new OverlappingMarkerSpiderfier(map, {keepSpiderfied: true});
+
 
 $("#form_location").submit(function(event){
     $("#form_location").children('input[type=submit]').attr('disabled',
@@ -33,14 +41,8 @@ $("#form_location").submit(function(event){
 	map.setCenter(latlon);
 	map.setZoom(8);
 	console.log("YO I GOT HERE");
-	/*	var usualColor = 'eebb22';
-		var spiderfiedColor = 'ffee22';
-		var iconWithColor = function(color) {
-		return 'http://chart.googleapis.com/chart?chst=d_map_xpin_letter&chld=pin|+|' +
-		color + '|000000|ffff00';
-		}*/
 	oms.addListener('click', function(marker) {
-		    
+	    
 	});
 	EchoServer.obtainArtists(lat, lon, function(artists) {
 	    $("#notif").html("&nbsp");
@@ -59,38 +61,43 @@ $("#form_location").submit(function(event){
 
 
 		});
-	/*	oms.addListener('unspiderfy', function(markers) {
+		/*	oms.addListener('unspiderfy', function(markers) {
 
-		});*/
+			});*/
 		
 		google.maps.event.addListener(marker, 'click', function(event){
-		  var info = "<h1>"+this.artist.name+" (" + 
-		  this.artist.artist_location.location + ")</h1>";
-		  info += "<ul>"
-		  for (var i = 0; i < this.artist.songs.length; i++)
-		  info += "<li>" + this.artist.songs[i].title + "</li>";
-		  info += "</ul>";
-		  $("#songs").html(info);
-		  event.stop();
-		  });
+		    var info = "<h1>"+this.artist.name+" (" + 
+			this.artist.artist_location.location + ")</h1>";
+		    info += "<ul>"
+		    for (var i = 0; i < this.artist.songs.length; i++)
+			info += "<li>" + this.artist.songs[i].title + "</li>";
+		    info += "</ul>";
+		    $("#songs").html(info);
+		    event.stop();
+		});
 	    }
 	    $("#form_location").children('input[type=submit]').removeAttr('disabled');
 	});
     });
 });
 
-    MusicMap = {};
-    MusicMap.extractLocation = function(input, callback) {
-	var regex = new RegExp("[-+]?[0-9]*\.?[0-9]+,[-+]?[0-9]*\.?[0-9]+");
-	var parsed_input = input.match(regex);
-	if (parsed_input) {
-	    parsed_input = parsed_input[0].split(",");
-	    callback(parsed_input[0], parsed_input[1]);
-	} else {
-	    var geocoder = new google.maps.Geocoder();
-	    geocoder.geocode({'address':input}, function(results, status) {
-		callback(results[0].geometry.location.lat(),
-			 results[0].geometry.location.lng());
-	    });
-	}
-    };
+
+
+MusicMap = {};
+MusicMap.extractLocation = function(input, callback) {
+    // Determine if the location is a coordinate
+    var regex = new RegExp("[-+]?[0-9]*\.?[0-9]+,[-+]?[0-9]*\.?[0-9]+");
+    var parsed_input = input.match(regex);
+    if (parsed_input) {
+	// location is a coordinate
+	parsed_input = parsed_input[0].split(",");
+	callback(parsed_input[0], parsed_input[1]);
+    } else {
+	// location is not a coordinate
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address':input}, function(results, status) {
+	    callback(results[0].geometry.location.lat(),
+		     results[0].geometry.location.lng());
+	});
+    }
+};
